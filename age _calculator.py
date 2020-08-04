2740f0123f5d3c6785d1e41f8ba8cfacb00eefe6
Praveen Dubey <69203092+PraveenDubey01@users.noreply.github.com>
2020-08-04 19:17:30 +0530

    Update age _calculator.py

--- age _calculator.py
+++ age _calculator.py
@@ -1,4 +1,4 @@
 import datetime
-birthyear = int(input("ENTER YOUR BIRTH YEAR : "))
+birthyear = int(input("ENTER YOUR BIRTH YEAR : "))        # enter the current/Ongoing YEAR
 age = datetime.datetime.now().year-birthyear
 print("YOUR AGE IS", age , "YEARS OLD")
